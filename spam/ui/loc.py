3705623c4203f0799e6772d09f58ab1bd19501e8
jeffwong728 <wwong728@163.com>
2018-11-15 19:58:01 +0800

    Lines of code count script.

---
+++ spam/ui/loc.py
@@ -0,0 +1,14 @@
+﻿import os
+import sys
+
+thisDir = os.path.dirname(os.path.abspath(sys.argv[0]))
+ln = 0
+for root, dirs, files in os.walk(thisDir, topdown=True, followlinks=False):
+    for file in files:
+        fullPath = os.path.join(root, file)
+        if fullPath.endswith(".h") or fullPath.endswith(".cpp"):
+            with open(fullPath, encoding='utf-8') as f:
+                for l in f:
+                    ln += 1
+print("There are " + str(ln) + " lines of code")
+os.system("pause")
