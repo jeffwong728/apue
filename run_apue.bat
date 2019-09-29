d44fe09da4def4baef39b5db142233a8ca77108a
jeffwong728 <wwong728@163.com>
2019-09-29 23:02:58 +0800

    1. CV python 3 support.

--- run_apue.bat
+++ run_apue.bat
@@ -3,6 +3,7 @@
 @set PATH=%PATH%;D:\apue\install\tbb\bin\intel64\vc14
 @set PATH=%PATH%;D:\apue\install\opencv\x64\vc15\bin
 @set PATH=%PATH%;D:\apue\install\apue\bin
+@set PATH=%PATH%;D:\apue\install\OpenBLAS\bin
 cd build
 cd apue
 cd spam
