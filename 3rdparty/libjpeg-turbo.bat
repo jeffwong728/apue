d5ceab70fefc6e39183b62067c2e5733757db869
jeffwong728 <wwong728@163.com>
2019-10-07 12:06:45 +0800

    1. 3rdparty build updates.

--- 3rdparty/libjpeg-turbo.bat
+++ 3rdparty/libjpeg-turbo.bat
@@ -3,6 +3,6 @@ cd ..
 cd msvs
 mkdir "libjpeg-turbo"
 cd "libjpeg-turbo"
-cmake -G"Visual Studio 15 2017 Win64" -C"../../3rdparty/cmake/libjpeg-turbo.cmake" "../../3rdparty/libjpeg-turbo-2.0.0"
+cmake -G"Visual Studio 15 2017 Win64" -C"../../3rdparty/cmake/libjpeg-turbo.cmake" "../../3rdparty/libjpeg-turbo-2.0.3"
 cmake -LA -N >cfg.txt
 @%comspec% /k
