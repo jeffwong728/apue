bd611c9cdb826fd2c88f4e6e977097e4ea3cf27e
jeffwong728 <wwong728@163.com>
2019-10-07 12:32:44 +0800

    1. Freeglut update to 3.2.1.

--- 3rdparty/freeglut.bat
+++ 3rdparty/freeglut.bat
@@ -3,6 +3,6 @@ cd ..
 cd msvs
 mkdir freeglut
 cd freeglut
-cmake -G"Visual Studio 15 2017 Win64" -C"../../3rdparty/cmake/freeglut.cmake" "../../3rdparty/freeglut-3.0.0"
+cmake -G"Visual Studio 15 2017 Win64" -C"../../3rdparty/cmake/freeglut.cmake" "../../3rdparty/freeglut-3.2.1"
 cmake -LA -N >cfg.txt
 @%comspec% /k
