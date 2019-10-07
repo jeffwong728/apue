4f3b9948ea88289043b2a3f9e012322aa897483a
jeffwong728 <wwong728@163.com>
2019-10-07 22:34:59 +0800

    1. Third libraries updates.

--- 3rdparty/pcre.bat
+++ 3rdparty/pcre.bat
@@ -3,6 +3,6 @@ cd ..
 cd msvs
 mkdir pcre
 cd pcre
-cmake -G"Visual Studio 15 2017 Win64" -C"../../3rdparty/cmake/pcre.cmake" "../../3rdparty/pcre-8.42"
-cmake -LA -N >cfg.txt
+cmake -G"Visual Studio 15 2017 Win64" -C"../../3rdparty/cmake/pcre.cmake" "../../3rdparty/pcre-8.43"
+cmake -LA -N . >cfg.txt
 @%comspec% /k
