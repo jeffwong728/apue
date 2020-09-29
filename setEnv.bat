b73006e8318332fddbc84f241fc01ce293d0346e
jeffwong728 <wwong728@163.com>
2020-09-29 19:35:50 +0800

    1. VS Code workspace creation.

--- setEnv.bat
+++ setEnv.bat
@@ -1,4 +1,3 @@
-@set "SPAM_ROOT_DIR=%~dp0"
 @set PYTHONIOENCODING=utf-8
 @set PATH=C:\Program Files\CMake\bin;%PATH%
 @set PATH=D:\apue\install\opencv\x64\vc15\bin;%PATH%
@@ -7,5 +6,4 @@
 @set PATH=D:\apue\install\simd\bin;%PATH%
 @set PATH=D:\apue\install\wxWidgets\lib\vc_x64_dll;%PATH%
 @set VCPKG_DIR=%VCPKG_ROOT_DIR%/installed/x64-windows
-@set OpenBLAS_HOME=D:/apue/install/OpenBLAS
 @rem set PYTHONPATH=D:\apue\msvs\opencv\python_loader;%PYTHONPATH%
