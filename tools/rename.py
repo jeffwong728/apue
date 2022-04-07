11b16f2a33a3900d8fbf5f2b343c75ac2539c780
weiwang <wwang728@gmail.com>
2022-04-07 10:16:28 +0800

    Add a rename tool script.

---
+++ tools/rename.py
@@ -0,0 +1,15 @@
+﻿import os
+import sys
+
+thisDir = os.path.dirname(os.path.abspath(sys.argv[0]))
+ln = 0
+for root, dirs, files in os.walk(thisDir, topdown=True, followlinks=False):
+    for file in files:
+        if file.endswith(".CPP") and file.upper() == file:
+            r = input("Rename " + file + " to " + file.lower() + "? (Y or N): ")
+            r = r.strip()
+            r = r.lower()
+            if not r or r=="y":
+                print("Rename " + file + " to " + file.lower())
+                os.rename(os.path.join(root, file), os.path.join(root, file.lower()))
+os.system("pause")
