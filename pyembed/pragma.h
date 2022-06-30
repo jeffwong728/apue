de438496b5a2a47821c07958fa2eb9f6d42c282a
weiwang <wwang728@gmail.com>
2022-06-30 19:40:53 +0800

    Log embed python method.

---
+++ pyembed/pragma.h
@@ -0,0 +1,3 @@
+#pragma comment(linker, "/manifestdependency:\"name='python3' "\
+                        "processorArchitecture='amd64' version='1.0.0.0' "\
+                        "type='win32'\"")
